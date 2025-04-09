a5e0dd262cf4b6438be08c27e32eed35c7a62934
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2025-04-08 22:07:51 -0300

    update on the componente

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,8 @@
+export default function SearchBar() {
+  return (
+    <form action="">
+      <input type="text" placeholder="digite sua busca" />
+      <button type="submit">Buscar</button>
+    </form>
+  );
+}
